f9d0f71eabae9cfd70f39724fb304d79be67c316
TH2207 <member_all_out@yahoo.co.jp>
2023-01-15 12:42:09 +0900

    first-commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+function scrollTop(elem) {
+  const target = document.getElementById(elem);
+  target.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+}
+
+function hamburgerMenu(elem) {
+  const target = document.getElementById(elem);
+  target.addEventListener('click', () => {
+    target.classList.toggle('open');
+    const nav = document.getElementById("drawer-nav");
+    nav.classList.toggle('in');
+  });
+}
+
+scrollTop('button');
+hamburgerMenu('menu');
+
+const openBtn = document.getElementById('openBtn');
+const closeBtn = document.getElementById('closeBtn');
+const modal = document.getElementById('modal');
+openBtn.addEventListener('click', () => {
+  modal.style.display = 'block';
+})
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+})
+window.addEventListener('click', (e) => {
+  if (!e.target.closest('.modal__content-inner') && e.target.id !== "openBtn") {
+    modal.style.display = 'none';
+  }
+});
